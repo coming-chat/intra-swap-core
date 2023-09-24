3ea52209f7d3cc4ea459d309ae62212f9c1c01c3
Gkirito <lownkid@outlook.com>
2023-09-25 00:30:28 +0800

    feat(cmd): update base

--- cmd/base.go
+++ cmd/base.go
@@ -11,7 +11,6 @@ import (
 	"github.com/coming-chat/intra-swap-core/routers/alpha_router"
 	gasModelsV2 "github.com/coming-chat/intra-swap-core/routers/alpha_router/models/v2"
 	gasModelsV3 "github.com/coming-chat/intra-swap-core/routers/alpha_router/models/v3"
-	"github.com/coming-chat/intra-swap-core/util"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -33,10 +32,10 @@ func Ready(chainId base_entities.ChainId) (*alpha_router.AlphaRouter, error) {
 		MultiCall2ProviderCore:   uniswapMultiCallCore,
 		V3IndexerProvider:        v3.NewGeckoTerminalProvider(chainId),
 		V3PoolProvider:           v3.NewBasePoolProvider(chainId, map[string]string{}, uniswapMultiCallCore, nil),
-		V3QuoteProvider:          v3.NewBaseQuoteProvider(context.TODO(), util.UniswapV3Quoter, chainId, rpcBaseProvider, uniswapMultiCallCore, nil, nil),
+		V3QuoteProvider:          v3.NewBaseQuoteProvider(context.TODO(), chainId, rpcBaseProvider, uniswapMultiCallCore, nil, false, nil),
 		V2IndexerProvider:        v2.NewGeckoTerminalProvider(chainId),
 		V2PoolProvider:           v2.NewBasePoolProvider(chainId, map[string]string{}, uniswapMultiCallCore, nil),
-		V2QuoteProvider:          v2.NewBaseQuoteProvider(),
+		V2QuoteProvider:          v2.NewBaseQuoteProvider(context.TODO(), chainId, rpcBaseProvider, uniswapMultiCallCore, false, nil),
 		TokenProvider:            providers.NewBaseTokenProvider(chainId, uniswapMultiCallCore),
 		GasPriceProvider:         &gas.BaseGasPriceProvider{},
 		V3GasModelFactory:        gasModelsV3.NewHeuristicGasModelFactory(rpcBaseProvider, uniswapMultiCallCore, ""),
