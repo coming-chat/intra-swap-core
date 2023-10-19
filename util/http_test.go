98d21adf62ed622bacd80317be66e2516648d175
Gkirito <lownkid@outlook.com>
2023-10-19 14:13:09 +0800

    test(util): add test case for util.Request

--- util/http_test.go
+++ util/http_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"net/http"
 	"testing"
 )
 
@@ -13,4 +14,15 @@ func TestGetReq(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(resp)
+}
+
+func TestRequest(t *testing.T) {
+	res, err := Request[map[string]interface{}, map[string]interface{}](http.MethodGet, "https://api.geckoterminal.com/api/v2/search/pools?query=ETH&network=base", map[string]string{
+		"Accept":     "application/json;version=20230302",
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+	}, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(res)
 }
